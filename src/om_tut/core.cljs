ac8508303c6fff3dfe8985feadc4b3ee278c9268
Anil Wadghule <anildigital@gmail.com>
2015-02-15 13:13:45 +0530

    Commit 3

--- src/om_tut/core.cljs
+++ src/om_tut/core.cljs
@@ -3,7 +3,9 @@
   (:require [figwheel.client :as fw]
             [om.core :as om :include-macros true]
             [om.dom :as dom :include-macros true]
-            [cljs.core.async :refer [put! chan <!]]))
+            [cljs.core.async :refer [put! chan <!]]
+            [clojure.data :as data]
+            [clojure.string :as string]))
 
 
 (enable-console-print!)
@@ -24,6 +26,26 @@
       {:first "Lem" :middle-initial "E" :last "Tweakit" :email "morebugs@mit.edu"}]}))
 
 
+
+(defn parse-contact [contact-str]
+  (let [[first middle last :as parts] (string/split contact-str #"\s+")
+        [first last middle] (if (nil? last) [first middle] [first last middle])
+        middle (when middle (string/replace middle "." ""))
+        c (if middle (count middle) 0)]
+    (when (>= (count parts) 2)
+      (cond-> {:first first :last last}
+        (== c 1) (assoc :middle-initial middle)
+        (>= c 2) (assoc :middle middle)))))
+
+(defn add-contact [data owner]
+  (let [new-contact (-> (om/get-node owner "new-contact")
+                        .-value
+                        parse-contact)]
+    (when new-contact
+      (om/transact! data :contacts #(conj % new-contact))
+      (om/set-state! owner :text ""))))
+
+
 (defn middle-name [{:keys [middle middle-initial]}]
   (cond
     middle (str " " middle)
@@ -42,26 +64,38 @@
         (dom/button #js {:onClick (fn [e] (put! delete @contact))} "Delete")))))
 
 
+(defn handle-change [e owner {:keys [text]}]
+  (let [value (.. e -target -value)]
+    (if-not (re-find #"[0-9]" value)
+      (om/set-state! owner :text value)
+      (om/set-state! owner :text text))))
+
 (defn contacts-view [data owner]
   (reify
     om/IInitState
     (init-state [_]
-      {:delete (chan)})
+      {:delete (chan)
+       :text ""})
     om/IWillMount
     (will-mount [_]
       (let [delete (om/get-state owner :delete)]
         (go (loop []
-          (let [contact (<! delete)]
-            (om/transact! data :contacts
-              (fn [xs] (vec (remove #(= contact %) xs))))
-            (recur))))))
+              (let [contact (<! delete)]
+                (om/transact! data :contacts
+                  (fn [xs] (vec (remove #(= contact %) xs))))
+                (recur))))))
     om/IRenderState
-    (render-state [this {:keys [delete]}]
+    (render-state [this state]
       (dom/div nil
         (dom/h2 nil "Contact list")
         (apply dom/ul nil
           (om/build-all contact-view (:contacts data)
-            {:init-state {:delete delete}}))))))
+            {:init-state state}))
+        (dom/div nil
+          (dom/input 
+            #js {:type "text" :ref "new-contact" :value (:text state)
+                 :onChange #(handle-change % owner state)})
+          (dom/button #js {:onClick #(add-contact data owner)} "Add contact"))))))
 
 
 (om/root contacts-view app-state
